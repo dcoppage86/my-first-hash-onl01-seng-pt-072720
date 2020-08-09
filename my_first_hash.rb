fe1c2fed325d96e75d3bd64385defb4301275227
Dakota Coppage <dakotakcoppage@me.com>
2020-08-09 17:02:38 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -9,7 +9,6 @@ end
 
 def retrieval(shipping_manifest)
   shipping_manifest = {"whale bone corsets" => 5,"porcelain vases" => 2,"oil paintings" => 3}
-end
   shipping_manifest["oil paintings"]
 end
 
