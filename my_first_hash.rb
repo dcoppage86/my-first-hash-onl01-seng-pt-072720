5372b941ad3765f7302c6ec2ab918390ab6a726b
Dakota Coppage <dakotakcoppage@me.com>
2020-08-09 17:02:26 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -9,9 +9,8 @@ end
 
 def retrieval(shipping_manifest)
   shipping_manifest = {"whale bone corsets" => 5,"porcelain vases" => 2,"oil paintings" => 3}
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-shipping_manifest["oil paintings"]
+end
+  shipping_manifest["oil paintings"]
 end
 
 def adding
